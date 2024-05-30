ca59fc2f3a33d3dabd9a4604acf371030b911d4f
Rong Liu <102014841+RongLiu-Leo@users.noreply.github.com>
2024-05-30 01:44:53 -0400

    add training monitor and result viewer code

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -10,6 +10,8 @@
 #
 
 import torch
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
 
 def mse(img1, img2):
     return (((img1 - img2)) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
@@ -17,3 +19,43 @@ def mse(img1, img2):
 def psnr(img1, img2):
     mse = (((img1 - img2)) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
     return 20 * torch.log10(1.0 / torch.sqrt(mse))
+
+def gradient_map(image):
+    sobel_x = torch.tensor([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]).float().unsqueeze(0).unsqueeze(0).cuda()/4
+    sobel_y = torch.tensor([[-1, -2, -1], [0, 0, 0], [1, 2, 1]]).float().unsqueeze(0).unsqueeze(0).cuda()/4
+    
+    grad_x = torch.cat([F.conv2d(image[i].unsqueeze(0), sobel_x, padding=1) for i in range(image.shape[0])])
+    grad_y = torch.cat([F.conv2d(image[i].unsqueeze(0), sobel_y, padding=1) for i in range(image.shape[0])])
+    magnitude = torch.sqrt(grad_x ** 2 + grad_y ** 2)
+    magnitude = magnitude.norm(dim=0, keepdim=True)
+
+    return magnitude
+
+def colormap(map, cmap="turbo"):
+    colors = torch.tensor(plt.cm.get_cmap(cmap).colors).to(map.device)
+    map = (map - map.min()) / (map.max() - map.min())
+    map = (map * 255).round().long().squeeze()
+    map = colors[map].permute(2,0,1)
+    return map
+
+def render_net_image(render_pkg, render_items, render_mode, camera):
+    output = render_items[render_mode].lower()
+    if output == 'alpha':
+        net_image = render_pkg["rend_alpha"]
+    elif output == 'normal':
+        net_image = render_pkg["rend_normal"]
+        net_image = (net_image+1)/2
+    elif output == 'depth':
+        net_image = render_pkg["surf_depth"]
+    elif output == 'edge':
+        net_image = gradient_map(render_pkg["render"])
+    elif output == 'curvature':
+        net_image = render_pkg["rend_normal"]
+        net_image = (net_image+1)/2
+        net_image = gradient_map(net_image)
+    else:
+        net_image = render_pkg["render"]
+
+    if net_image.shape[0]==1:
+        net_image = colormap(net_image)
+    return net_image
