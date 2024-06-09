1716c7834a33bd88428bd4657c0bcfe08a35851d
hbb1 <huangbb@shanghaitech.edu.cn>
2024-06-09 23:17:06 +0800

    Merge branch 'main' of https://github.com/RongLiu-Leo/2d-gaussian-splatting into RongLiu-Leo-main

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -54,6 +54,7 @@ class ModelParams(ParamGroup):
         self._white_background = False
         self.data_device = "cuda"
         self.eval = False
+        self.render_items = ['RGB', 'Alpha', 'Normal', 'Depth', 'Edge', 'Curvature']
         super().__init__(parser, "Loading Parameters", sentinel)
 
     def extract(self, args):
