23012695a154b6c96f27c234ba0c24bdd92d1bc6
hugoycj <576550489@qq.com>
2024-10-18 06:51:49 +0800

    fix: Update training scripts

--- scripts/train_with_mask.sh
+++ scripts/train_with_mask.sh
@@ -1,9 +1,12 @@
 data=$1
-
-gs-init -s $data -o ${data}_gaustudio_wmask --w_mask mask
-python train_progressive.py -s ${data}_gaustudio_wmask -r 1  --lambda_dist 1000 \
-        --w_mask masks --lambda_mask 0.1 --max_screen_size 5 -m ${data}_gaustudio_wmask/result \
-        --position_lr_init 0.000016 --iteration 20000
-gs-extract-pcd -m ${data}_gaustudio_wmask/result -o ${data}_gaustudio_wmask/result/fusion --meshing poisson
-texrecon ${data}_gaustudio_wmask/result/fusion/images ${data}_gaustudio_wmask/result/fusion/fused_mesh.ply \
-        ${data}_gaustudio_wmask/result/fusion/textured_mesh --outlier_removal=gauss_clamping --data_term=area --no_intermediate_results
+postix=gaustudio_wmask
+gs-init -s $data -o ${data}_${postix} --w_mask mask
+rm -r ${data}_${postix}/result_2
+python train_progressive.py -s ${data}_${postix} -r 1  --lambda_dist 1000 \
+        --w_mask masks --lambda_mask 0.1 --max_screen_size 5 \
+        -m ${data}_${postix}/result_2 --iteration 20000
+gs-extract-pcd -m ${data}_${postix}/result_2 -o ${data}_${postix}/result_2/fusion \
+        --meshing sap --config 2dgs
+texrecon ${data}_${postix}/result_2/fusion/images ${data}_${postix}/result_2/fusion/fused_mesh.ply \
+        ${data}_${postix}/result_2/fusion/textured_mesh --outlier_removal=gauss_clamping \
+        --data_term=area --no_intermediate_results
