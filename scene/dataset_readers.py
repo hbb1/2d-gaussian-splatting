ea966ec07d80ccfef60f10c522a11b588920bd49
Seiun <71683693+Zerui-Yu@users.noreply.github.com>
2025-06-12 14:35:25 +0800

    Update dataset_readers.py
    Fixed a simple bug about masks in the nerf_synthetic dataset

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -207,7 +207,9 @@ def readCamerasFromTransforms(path, transformsfile, white_background, extension=
 
             norm_data = im_data / 255.0
             arr = norm_data[:,:,:3] * norm_data[:, :, 3:4] + bg * (1 - norm_data[:, :, 3:4])
-            image = Image.fromarray(np.array(arr*255.0, dtype=np.byte), "RGB")
+            alpha = norm_data[:, :, 3:4]
+            rgba = np.concatenate([arr, alpha], axis=2)
+            image = Image.fromarray(np.array(rgba * 255.0, dtype=np.uint8), "RGBA")
 
             fovy = focal2fov(fov2focal(fovx, image.size[0]), image.size[1])
             FovY = fovy 
